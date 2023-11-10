e88c21851f55f81563c9d41770815a1afc7cb8b9
claylolx <claylolxqt@gmail.com>
2023-11-10 10:41:28 +0100

    Programa actualizado + .exe preparado y funional v2.0.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ internal class Program
         int numeroUno, numeroDos, total;
         Console.Write("Dame el primer numero: ");
         numeroUno = Convert.ToInt32(Console.ReadLine());
+
         Console.Write("Dame el segundo numero: ");
         numeroDos = Convert.ToInt32(Console.ReadLine());
+        
         total = numeroUno + numeroDos;
         Console.WriteLine("La suma de "+ numeroUno +" y "+ numeroDos +" da un total de "+ total);
+
+        Console.WriteLine("Presiona una tecla para cerrar la aplicación.");
+        Console.ReadKey();
     }
 }
