815048d04569c0d098e861aaab081244966bd5f3
claylolx <claylolxqt@gmail.com>
2023-11-10 10:32:52 +0100

    Programa sencillo para practica finalizado v1.0.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numeroUno, numeroDos, total;
+        Console.Write("Dame el primer numero: ");
+        numeroUno = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Dame el segundo numero: ");
+        numeroDos = Convert.ToInt32(Console.ReadLine());
+        total = numeroUno + numeroDos;
+        Console.WriteLine("La suma de "+ numeroUno +" y "+ numeroDos +" da un total de "+ total);
+    }
+}
